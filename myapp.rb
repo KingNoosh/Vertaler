dc25cfb834bca415b58becea787a1dfd539b82bd
Anosh Malik <me@ano.sh>
2016-10-01 23:52:16 +0100

    Refactor to SMS app

--- myapp.rb
+++ myapp.rb
@@ -1,18 +1,110 @@
 require "sinatra"
 require "twilio-ruby"
+require "google/cloud"
 
-post '/twiml' do
+gcloud = Google::Cloud.new
+hashLanguages = {
+  'Afrikaans'           => 'af'
+  'Albanian'            => 'sq'
+  'Arabic'              => 'ar'
+  'Armenian'            => 'hy'
+  'Azerbaijani'         => 'az'
+  'Basque'              => 'eu'
+  'Belarusian'          => 'be'
+  'Bengali'             => 'bn'
+  'Bosnian'             => 'bs'
+  'Bulgarian'           => 'bg'
+  'Catalan'             => 'ca'
+  'Cebuano'             => 'ceb'
+  'Chichewa'            => 'ny'
+  'Chinese Simplified'  => 'zh-CN'
+  'Chinese Traditional' => 'zh-TW'
+  'Croatian'            => 'hr'
+  'Czech'               => 'cs'
+  'Danish'              => 'da'
+  'Dutch'               => 'nl'
+  'English'             => 'en'
+  'Esperanto'           => 'eo'
+  'Estonian'            => 'et'
+  'Filipino'            => 'tl'
+  'Finnish'             => 'fi'
+  'French'              => 'fr'
+  'Galician'            => 'gl'
+  'Georgian'            => 'ka'
+  'German'              => 'de'
+  'Greek'               => 'el'
+  'Gujarati'            => 'gu'
+  'Haitian Creole'      => 'ht'
+  'Hausa'               => 'ha'
+  'Hebrew'              => 'iw'
+  'Hindi'               => 'hi'
+  'Hmong'               => 'hmn'
+  'Hungarian'           => 'hu'
+  'Icelandic'           => 'is'
+  'Igbo'                => 'ig'
+  'Indonesian'          => 'id'
+  'Irish'               => 'ga'
+  'Italian'             => 'it'
+  'Japanese'            => 'ja'
+  'Javanese'            => 'jw'
+  'Kannada'             => 'kn'
+  'Kazakh'              => 'kk'
+  'Khmer'               => 'km'
+  'Korean'              => 'ko'
+  'Lao'                 => 'lo'
+  'Latin'               => 'la'
+  'Latvian'             => 'lv'
+  'Lithuanian'          => 'lt'
+  'Macedonian'          => 'mk'
+  'Malagasy'            => 'mg'
+  'Malay'               => 'ms'
+  'Malayalam'           => 'ml'
+  'Maltese'             => 'mt'
+  'Maori'               => 'mi'
+  'Marathi'             => 'mr'
+  'Mongolian'           => 'mn'
+  'Myanmar (Burmese)'   => 'my'
+  'Nepali'              => 'ne'
+  'Norwegian'           => 'no'
+  'Persian'             => 'fa'
+  'Polish'              => 'pl'
+  'Portuguese'          => 'pt'
+  'Punjabi'             => 'ma'
+  'Romanian'            => 'ro'
+  'Russian'             => 'ru'
+  'Serbian'             => 'sr'
+  'Sesotho'             => 'st'
+  'Sinhala'             => 'si'
+  'Slovak'              => 'sk'
+  'Slovenian'           => 'sl'
+  'Somali'              => 'so'
+  'Spanish'             => 'es'
+  'Sudanese'            => 'su'
+  'Swahili'             => 'sw'
+  'Swedish'             => 'sv'
+  'Tajik'               => 'tg'
+  'Tamil'               => 'ta'
+  'Telugu'              => 'te'
+  'Thai'                => 'th'
+  'Turkish'             => 'tr'
+  'Ukrainian'           => 'uk'
+  'Urdu'                => 'ur'
+  'Uzbek'               => 'uz'
+  'Vietnamese'          => 'vi'
+  'Welsh'               => 'cy'
+  'Yiddish'             => 'yi'
+  'Yoruba'              => 'yo'
+  'Zulu'                => 'zu'
+}
+post '/' do
+  puts params
+  sender   = params['From']
+  body     = params['Body']
+  arrBody  = /(.*) in (.*)/.match(body)
+  phrase   = arrBody[1]
+  language = arrBody[2]
+  country  = hashLanguages[language]
   Twilio::TwiML::Response.new do |r|
-    # Use <Say> to give the caller some instructions
-    r.Say 'Welcome to Vertaler, the voice translation service.', voice: 'alice'
-    r.Say 'Please tell me what you want to hear and in what language.', voice: 'alice'
-    r.Say 'For example, if you want to know how to say Hello in French.', voice: 'alice'
-    r.Say 'Say hello in French.', voice: 'alice'
-
-    # Use <Record> to record the caller's message
-    r.Record
-
-    # End the call with <Hangup>
-    r.Hangup
+    r.Message 'Welcome to Vertaler, the voice translation service.'
   end.text
 end
